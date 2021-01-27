d942cddc6815414f782bf9c7a34a70231747135c
Jeremy <71183843+MyNameJeremy@users.noreply.github.com>
2021-01-27 08:51:41 +0100

    penup and pendown
    use arrow up and arrow down to start and stop drawing. the keys sometimes cause the turtle to move and I don't know why but I'll see what I can do.

--- KeyboardAsDrawingTablet.py
+++ KeyboardAsDrawingTablet.py
@@ -3,23 +3,29 @@ import turtle
 t = turtle.Turtle()
 turtle.Screen().setup(1104, 408)
 while True:
-    if keyboard.is_pressed("1"):
+    if keyboard.is_pressed("esc"):
+        break
+    elif keyboard.is_pressed("up"):
+        t.penup()
+    elif keyboard.is_pressed("down"):
+        t.pendown()
+    elif keyboard.is_pressed("1"):
+        t.goto(-500,100)
+    elif keyboard.is_pressed("2"):
+        t.goto(-400,100)
+    elif keyboard.is_pressed("3"):
+        t.goto(-300,100)
+    elif keyboard.is_pressed("4"):
+        t.goto(-200,100)
+    elif keyboard.is_pressed("5"):
+        t.goto(-100,100)
+    elif keyboard.is_pressed("6"):
+        t.goto(0,100)
+    elif keyboard.is_pressed("7"):
         t.goto(100,100)
-    if keyboard.is_pressed("2"):
+    elif keyboard.is_pressed("8"):
         t.goto(200,100)
-    if keyboard.is_pressed("3"):
+    elif keyboard.is_pressed("9"):
         t.goto(300,100)
-    if keyboard.is_pressed("4"):
+    elif keyboard.is_pressed("0"):
         t.goto(400,100)
-    if keyboard.is_pressed("5"):
-        t.goto(500,100)
-    if keyboard.is_pressed("6"):
-        t.goto(600,100)
-    if keyboard.is_pressed("7"):
-        t.goto(700,100)
-    if keyboard.is_pressed("8"):
-        t.goto(800,100)
-    if keyboard.is_pressed("9"):
-        t.goto(900,100)
-    if keyboard.is_pressed("0"):
-        t.goto(1000,100)
