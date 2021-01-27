80cb38381b419332bba0fe546ecde20c63717ab0
Jeremy <71183843+MyNameJeremy@users.noreply.github.com>
2021-01-27 08:27:41 +0100

    Penup/Pendown
    You can now press the "e" key, to start and stop drawing. sometimes it doesn't really stop or start because the loop already ran through again and set the value back to what it was before because the key was still pressed. I'm gonna look around if I find a way to fix that.

--- DrawingWithKeyboard.py
+++ DrawingWithKeyboard.py
@@ -1,8 +1,13 @@
 import keyboard
 import turtle
+pen = True
 t = turtle.Turtle()
 turtle.getscreen()
 while True:
+    if pen == True:
+        t.pendown()
+    else:
+        t.penup()
     if keyboard.is_pressed("q"):
         break
     if keyboard.is_pressed("w"):
@@ -13,4 +18,12 @@ while True:
         t.backward(10)
     if keyboard.is_pressed("d"):
         t.right(10)
-        
+    if keyboard.is_pressed("e"):
+        if pen == True:
+            pen = False
+        else:
+            pen = True
+        if pen == True:
+            t.pendown()
+        else:
+            t.penup()
